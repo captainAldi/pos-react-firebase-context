274069241a79b55aeccef5bfaad0b2862fb89a17
captainAldi <renaldiyulvianda@yahoo.com>
2020-07-26 20:49:38 +0700

    coba styling page

--- src/App.js
+++ src/App.js
@@ -5,28 +5,42 @@ import Login from './pages/Login';
 import ForgetPass from './pages/ForgetPass';
 import NotFound from './pages/NotFound';
 
+//Halaman Private
 import Private from './pages/private'
 import PrivateRoute from './components/PrivateRoute';
 
+//Firebase
 import FirebaseProvider from './components/FirebaseProvider';
+
+//Material UI
+import CssBaseline from '@material-ui/core/CssBaseline';
+import ThemeProvider from '@material-ui/styles/ThemeProvider'
+
+import theme from './config/Theme'
 
 function App() {
   return (
-    <FirebaseProvider>
-      <BrowserRouter>
-        <Switch>
-          <PrivateRoute path='/' exact component={Private} />
-          <PrivateRoute path='/pengaturan' component={Private} />
-          <PrivateRoute path='/produk' component={Private} />
-          <PrivateRoute path='/transaksi' component={Private} />
+    <>
+      <CssBaseline>
+        <ThemeProvider theme={theme}>
+          <FirebaseProvider>
+            <BrowserRouter>
+              <Switch>
+                <PrivateRoute path='/' exact component={Private} />
+                <PrivateRoute path='/pengaturan' component={Private} />
+                <PrivateRoute path='/produk' component={Private} />
+                <PrivateRoute path='/transaksi' component={Private} />
 
-          <Route path='/register' component={Register} />
-          <Route path='/login' component={Login} />
-          <Route path='/lupa-pass' component={ForgetPass} />
-          <Route component={NotFound} />
-        </Switch>
-      </BrowserRouter>
-    </FirebaseProvider>
+                <Route path='/register' component={Register} />
+                <Route path='/login' component={Login} />
+                <Route path='/lupa-pass' component={ForgetPass} />
+                <Route component={NotFound} />
+              </Switch>
+            </BrowserRouter>
+          </FirebaseProvider>
+       </ThemeProvider>
+      </CssBaseline>
+    </>
   );
 }
 
--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,11 +1,28 @@
 import React from 'react'
 
+import Button from '@material-ui/core/Button'
+
+import { makeStyles } from '@material-ui/core/styles'
+
 function Register() {
   return (
     <div>
-      <h1>Registrasi</h1>
+      <h1 style={styles.judul}>Registrasi</h1>
+      <Button
+        color="primary"
+        variant="contained"
+      >
+        Click !
+      </Button>
     </div>
   )
 }
+
+const styles = makeStyles(theme => ({
+    blue: {
+      color: theme.palette.primary.main
+    }
+  })
+)
 
 export default Register

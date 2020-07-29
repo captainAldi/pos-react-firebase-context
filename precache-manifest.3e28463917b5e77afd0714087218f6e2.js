3efca40a7e22e7f769967d0a4f17e588d1b0f3f9
captainAldi <renaldiyulvianda@yahoo.com>
2020-07-29 17:30:24 +0700

    Updates

---
+++ precache-manifest.3e28463917b5e77afd0714087218f6e2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49a7fd0663192190f9ecc22f973c8041",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6a3d7dcdec71b564af55",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "58a44a9673ff69d9010e",
+    "url": "/static/js/2.ab98e26e.chunk.js"
+  },
+  {
+    "revision": "fc94876ac19792aab3a41b65c9c4cb45",
+    "url": "/static/js/2.ab98e26e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a3d7dcdec71b564af55",
+    "url": "/static/js/main.0f909f98.chunk.js"
+  },
+  {
+    "revision": "4ed1229dc18570d6efb4",
+    "url": "/static/js/runtime-main.074f9c10.js"
+  }
+]);
